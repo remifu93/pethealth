8b208dd1bae4246744d358de716a6ef6ac095a14
Remigio Furini <remifafur@gmail.com>
2022-11-15 18:04:21 +0100

    crud vaccines with permissions admin

--- vaccine/views.py
+++ vaccine/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import Vaccine
 from .serializers import VaccineSerializer
 
@@ -6,3 +7,10 @@ from .serializers import VaccineSerializer
 class VaccineViewSet(viewsets.ModelViewSet):
     serializer_class = VaccineSerializer
     queryset = Vaccine.objects.all()
+
+    def get_permissions(self):
+        if self.action == 'list':
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
