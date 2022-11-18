c468b67fa6dd6e5bcc339d1617db769405539f6e
Remigio Furini <remifafur@gmail.com>
2022-11-18 14:17:30 +0100

    spanish translate

--- pethealth/settings.py
+++ pethealth/settings.py
@@ -129,7 +129,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'Chile/Continental'
 
