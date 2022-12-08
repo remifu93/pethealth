d59f88b499faa150cf9fbe612abccb0b7d68aef7
Remigio Furini <remifafur@gmail.com>
2022-12-08 16:23:05 +0100

    agregado cambios en el modelo mascotas

--- pet/models.py
+++ pet/models.py
@@ -1,13 +1,28 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 from base.models import TimeStampedModel
 from user.models import User
+from race.models import Race
 
 
 class Pet(TimeStampedModel):
+
+    class SexChoices(models.TextChoices):
+        MALE = 'ML', 'Macho'
+        FEMALE = 'FM', 'Hembra'
+
     name = models.CharField('Nombre', max_length=50)
     birth_date = models.DateField('Fecha de nacimiento')
     user = models.ForeignKey(User, verbose_name='Usuario', on_delete=models.CASCADE)
-    active = models.BooleanField('Active', default=True)
+    active = models.BooleanField('Activo', default=True)
+    race = models.ForeignKey(Race, verbose_name='Raza', on_delete=models.CASCADE)
+    sex = models.CharField(
+        max_length=2,
+        choices=SexChoices.choices,
+    )
+    weight = models.SmallIntegerField(
+        validators=[MaxValueValidator(100), MinValueValidator(1)]
+    )
 
     class Meta:
         verbose_name = "Mascota"
